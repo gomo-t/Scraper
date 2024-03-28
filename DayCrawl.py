d6c176ff3d7363cc66bea17f53fd20c2400eb912
Tendekayi Gomo <hebecozw@gmail.com>
2024-03-28 14:52:41 +0100

    Input date functionality

--- DayCrawl.py
+++ DayCrawl.py
@@ -1,12 +1,19 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 from crawl import SearchDate as sd
-
-# Define your date 
-query_date = datetime(2024, 3, 1)  # Change this to your desired start date
 
 # Define the date format string expected by the input field
 date_format = "%d/%m/%Y"
 
-#Search query
-search_date = query_date.strftime(date_format)
-sd(search_date)  
+# Get user input for the search date
+user_input = input("Enter the date of search (DD/MM/YYYY): ")
+
+try:
+    # Parse the user input into a datetime object using the specified format
+    search_date = datetime.strptime(user_input, date_format)
+except ValueError:
+    print("Invalid date format. Please enter the date in the format DD/MM/YYYY.")
+else:
+    # Call your search function with the formatted search date
+    search_date_str = search_date.strftime(date_format)
+    sd(search_date_str)
+
