fc0a3fdf4ca2ad538e89d83ae5680ee3d4a296a0
Tendekayi Gomo <hebecozw@gmail.com>
2024-03-30 14:54:53 +0100

    New main File:Execute.py added command line positional arguments

--- DayCrawl.py
+++ DayCrawl.py
@@ -1,19 +1,21 @@
 from datetime import datetime
 from crawl import SearchDate as sd
 
-# Define the date format string expected by the input field
-date_format = "%d/%m/%Y"
 
-# Get user input for the search date
-user_input = input("Enter the date of search (DD/MM/YYYY): ")
+def dayCrawl(inputDate):
+    # Define the date format string expected by the input field
+    date_format = "%d/%m/%Y"
 
-try:
-    # Parse the user input into a datetime object using the specified format
-    search_date = datetime.strptime(user_input, date_format)
-except ValueError:
-    print("Invalid date format. Please enter the date in the format DD/MM/YYYY.")
-else:
-    # Call your search function with the formatted search date
-    search_date_str = search_date.strftime(date_format)
-    sd(search_date_str)
+    # Get user input for the search date
+    user_input = inputDate#input("Enter the date of search (DD/MM/YYYY): ")
+
+    try:
+        # Parse the user input into a datetime object using the specified format
+        search_date = datetime.strptime(user_input, date_format)
+    except ValueError:
+        print("Invalid date format. Please enter the date in the format DD/MM/YYYY.")
+    else:
+        # Call your search function with the formatted search date
+        search_date_str = search_date.strftime(date_format)
+        sd(search_date_str)
 
---
+++ Executer.py
@@ -0,0 +1,34 @@
+from DayCrawl import dayCrawl as dc
+from Histcrawl import historyCrawl as hc
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+
+# Usage message for program
+parser.usage = """
+Use this program to perform sraping of daily and historical data from metalsmine.com/calendar
+
+-d is for news for that day eg -d 19/03/2009
+-s ,-e is when looking for historical data eg. -s 19/03/2009 e-20/03/2009
+
+"""
+
+#parser.add_argument("-d", "--daynews", action="store_true")
+parser.add_argument("-d", "--date", type=str, help="Specify the date in DD-MM-YYYY format for Day Crawl")
+parser.add_argument("-s", "--histStartDate", type=str, help="Specify the date in DD-MM-YYYY format for Histcrawl")
+parser.add_argument("-e", "--histEndDate", type=str, help="Specify the date in DD-MM-YYYY format for Histcrawl")
+
+args = parser.parse_args()
+
+# Accessing the date argument value
+if args.date:
+    specified_date = args.date
+    # Call the dayCrawl method with the specified date
+    dc(specified_date)
+if args.histStartDate:
+    histStartSpecified_date = args.histStartDate
+
+    histEndSpecified_date = args.histEndDate
+
+    #call the Histcrawl method with the specified dates
+    hc(histStartSpecified_date, histEndSpecified_date)
